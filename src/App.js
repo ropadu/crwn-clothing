1c547aecba1373fff9dc9ff82b8196516d772ebb
Radu Popa <>
2020-04-06 19:14:40 +0300

    created shop page, collection preview, collection item

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import Homepage from './pages/homepage/homepage.component'
+import {Switch, Route} from 'react-router-dom';
+import './App.css';
+
+import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 class App extends React.Component {
   constructor(){
@@ -10,8 +14,11 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-      <Homepage />
+      <div>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path ='/shop' component={ShopPage} />
+      </Switch>
       </div>
     );
 
